683c06cb5274514ecdadeaabeb03b5e00e3b1502
loochuynhh <lochuynhhsb@gmail.com>
2023-05-06 17:50:45 +0700

    add film

--- Admin/js/FilmManage.js
+++ Admin/js/FilmManage.js
@@ -1,13 +1,24 @@
 const URLFILM = "https://636b935c7f47ef51e13457fd.mockapi.io/product";
 const url = 'https://localhost:44308/api';
 const genre = 'genre';
-
+const addfilm = 'film';
 var addFilm = document.getElementById("addFilm");
 var overlay = document.getElementById("overlay");
 var overlayAddFilm = document.getElementById("overlayAddFilm")
 var btCancel = document.getElementById("btCancel");
 var btbtOK = document.getElementById("btOK");
-const genreListDiv = document.getElementById('genre-list');
+const genreListDiv1 = document.getElementById('genre-list1');
+
+// form.addEventListener('submit', handleSubmit);
+
+function showAlertTimeOut(message) {
+  document.getElementById("modal-message").innerHTML = message;
+  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+  myModal.show();
+  setTimeout(function(){
+    myModal.hide();
+  }, 2000);
+}
 
 function deleteUsers(i) {
     let url = "https://636b935c7f47ef51e13457fd.mockapi.io/product" + i;
@@ -78,6 +89,7 @@ function showAlert(message) {
     var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
     myModal.show();
 }
+
 function handleOutsideClickAddFilm(event) {
     if (!overlayAddFilm.contains(event.target)) {
         overlay.style.display = "none";
@@ -91,7 +103,6 @@ addFilm.addEventListener("click", function () {
     document.addEventListener("click", handleOutsideClickAddFilm, true);
 });
 
-console.log(localStorage.getItem('token'));
 
 fetch(`${url}/${genre}`, {
   method: 'GET',
@@ -101,22 +112,147 @@ fetch(`${url}/${genre}`, {
   })
   .then(response => response.json())
   .then(genres => {
-    console.log(genres);
+    var a = 0;
     genres.forEach(genre => {
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.name = genre.name;
       checkbox.value = genre.id;
       checkbox.id = `genre-${genre.id}`;
-
+      checkbox.style.marginRight = '15px';
+      // checkbox.style.marginLeft = '20px';
       const label = document.createElement('label');
       label.htmlFor = `genre-${genre.id}`;
       label.appendChild(document.createTextNode(genre.name));
-
-      genreListDiv.appendChild(checkbox);
-      genreListDiv.appendChild(label);
+      // checkbox.style.marginLeft = '50px';
+      label.style.width = '200px';
+      // checkbox.style.marginLeft = '25px';
+      genreListDiv1.appendChild(checkbox);
+      genreListDiv1.appendChild(label);
     });
+    
   });
 
+
+// fetch(`${url}/${addfilm}`, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': 'Bearer' +  localStorage.getItem('token')
+//   },
+//   body: JSON.stringify(
+//     { 
+//       id: 0,
+//       name: Name, 
+//       country: Country, 
+//       length: Length, 
+//       content: Content, 
+//       director: Director, 
+//       actor: Actor, 
+//       poster: Poster,
+//       trailer: Trailer,
+//       filmStatus: FilmStatus,
+//       ageLimit: AgeLimit, 
+//       genres: Genres
+//     }
+//   )
+// })
+// .then(response => {
+//   if (!response.ok) {
+//     showAlertTimeOut('Đăng nhập không thành công');
+//     throw new Error('Đã xảy ra lỗi khi thêm phim mới');
+//   }
+//   showAlertTimeOut('Đăng nhập thành công');
+// })
+// .catch(error => {
+//   console.error(error);
+// });
+
+const form = document.querySelector('#movie-form');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault(); // ngăn chặn form gửi đi và refresh trang
+
+  // kiểm tra các trường bắt buộc
+  // const requiredFields = ['name', 'country', 'actor', 'director', 'length', 'genre', 'age', 'content'];
+  // const missingFields = requiredFields.filter(field => {
+  //   const input = document.getElementById(field);
+  //   return !input.value;
+  // });
+
+  // if (missingFields.length > 0) {
+  //   alert(`Vui lòng điền đầy đủ các trường bắt buộc: ${missingFields.join(', ')}`);
+  //   return; // dừng lại nếu có trường bắt buộc chưa điền
+  // }
+
+  const name = document.getElementById('name').value;
+  const country = document.getElementById('country').value;
+  const actor = document.getElementById('actor').value;
+  const director = document.getElementById('director').value;
+  const trailer = document.getElementById('trailer').value;
+  const length = document.getElementById('length').value;
+  const ageLimit = document.getElementById('age').value;
+  const content = document.getElementById('content').value;
+  const poster = document.getElementById('poster').value;
+  const inputElement = document.getElementById("poster");
+  const Genres = [];
+  const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+  checkboxes.forEach((checkbox) => {
+    Genres.push({
+      id: checkbox.value,
+      name: checkbox.name
+    });
+  });
+  const ReleaseDate = document.getElementById('dateOfBirthRelease').value;
+  const filmStatus = "NOSCHEDULED";
+
+  // kiểm tra poster có được upload hay không
+  // let posterUrl = '';
+  // if (poster) {
+  //   // nếu có thì lưu vào server và lấy url
+  //   posterUrl = uploadPoster(poster);
+  // }
+
+//   // tạo object chứa thông tin phim
+  const newMovie = {
+    name,
+    country,
+    actor,
+    director,
+    length,
+    Genres,
+    ageLimit,
+    content,
+    trailer,
+    ReleaseDate,
+    filmStatus
+  };
+  const newMovieJson = JSON.stringify(newMovie);
+  const formData = new FormData();
+  const inputFile = document.querySelector('#poster');
+  formData.append('film',newMovieJson);
+  formData.append('poster', inputFile.files[0]);
+  formData.append('adposter', inputFile.files[0]);
+  formData.forEach((value, key) => {
+        console.log(`${key}: ${value}`);
+      });
+  fetch(`${url}/${addfilm}`, {
+    method: 'POST',
+    headers: {
+      // 'Content-Type': 'multipart/form-data',
+      'Authorization' : "bearer " + localStorage.getItem('token')
+    },
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      showAlertTimeOut('Thêm phim mới không thành công');
+      throw new Error('Đã xảy ra lỗi khi thêm phim mới');
+    }
+    showAlertTimeOut('Thêm phim mới thành công');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
 
 
--- js/HomePage.js
+++ js/HomePage.js
@@ -2,6 +2,9 @@ const url = 'https://localhost:44308/api';
 const Login = 'login';
 const signup = 'register';
 const genre = 'genre';
+const Users = `users`;
+const Password = "";
+const token = localStorage.getItem('token');
 
 var Overlay = document.getElementById("overlay");
 
@@ -14,6 +17,16 @@ var signupButton = document.getElementById("signupButton");
 var OverlaySignup = document.getElementById("overlaySignup");
 var homeLink = document.getElementById('home'); 
 var overlayAddFilm = document.getElementById('overlayAddFilm'); 
+var overlayUser = document.getElementById("overlayUser");
+var overlayHome = document.getElementById("overlayHome");
+var OverlayInfor = document.getElementById("overlayInfo");
+var dropdowninfo = document.getElementById("info");
+var dropdownlogout = document.getElementById("logout");
+var passwordChange = document.getElementById("passwordChange");
+var changePasswordForm = document.getElementById("changePasswordForm");
+var oldPassword = document.getElementById("oldPassword");
+var newPassword = document.getElementById("newPassword");
+var newPasswordConfirm = document.getElementById("newPasswordConfirm");
 
 function handleOutsideClickLogin(event) {
   if (!OverlayLogin.contains(event.target)) {
@@ -43,14 +56,60 @@ signupButton.addEventListener("click", function () {
   document.addEventListener("click", handleOutsideClickSignup, true);
 });
 
-
+// function showOverlay(title, message) {
+//   // Lấy thẻ modal
+//   var modal = $('#exampleModal')
+//   // Đặt tiêu đề cho modal
+//   modal.find('.modal-title').text(title)
+//   // Đặt thông báo cho modal
+//   modal.find('.modal-body #overlay-message').text(message)
+//   // Hiển thị modal
+//   modal.modal('show')
+//   // Tự động đóng overlay sau 2 giây
+//   setTimeout(function() {
+//     modal.modal('hide')
+//   }, 2000);
+// }
+function showOverlay(title, message) {
+  // Lấy thẻ modal
+  var modal = document.getElementById('exampleModal')
+  // Đặt tiêu đề cho modal
+  modal.querySelector('.modal-title').textContent = title
+  // Đặt thông báo cho modal
+  modal.querySelector('.modal-body #overlay-message').textContent = message
+  // Hiển thị modal
+  modal.classList.add('show')
+  modal.style.display = 'block'
+  modal.removeAttribute('aria-hidden')
+  setTimeout(function(){
+    modal.classList.remove('show')
+    modal.style.display = 'none'
+    modal.setAttribute('aria-hidden', 'true')
+  }, 2000)
+}
 function showAlert(message) {
+  document.getElementById("modal-message").innerHTML = message;
+  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+  myModal.show();
+}
+function showAlertVer2(message) {
   document.getElementById("modal-message").innerHTML = message;
   var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
   Overlay.style.display = "none";
   OverlayLogin.style.display = "none";
   OverlaySignup.style.display = "none";
   myModal.show();
+}
+function showAlertTimeOut(message) {
+  document.getElementById("modal-message").innerHTML = message;
+  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+  Overlay.style.display = "none";
+  OverlayLogin.style.display = "none";
+  OverlaySignup.style.display = "none";
+  myModal.show();
+  setTimeout(function(){
+    myModal.hide();
+  }, 800);
 }
 function showSuccess(){
   var alertSuccess = document.querySelector('.alert-success'); 
@@ -62,7 +121,6 @@ function showSuccess(){
     alertSuccess.style.display = 'none';
   }, 2000);
 }
-
 function showUnsuccess(){
   var alertDanger = document.querySelector('.alert-danger'); 
   alertDanger.style.display = 'block'; 
@@ -168,9 +226,14 @@ document.querySelector('#formLogin').addEventListener('submit', function(event) 
       })
       .then(response => {
           if(response.status == '403'){
-            window.location.assign("./Layoutuser.html");
+            showAlertTimeOut('Đăng nhập thành công');
+            overlayHome.style.display = "none";
+            overlayUser.style.display = "block";
+            Overlay.style.display = "none";
+            OverlayLogin.style.display = "none";
           }
           else if(response.status == '200'){
+            showAlertTimeOut('Đăng nhập thành công');
             window.location.assign("./Admin/Index.html");
           }
           
@@ -179,5 +242,190 @@ document.querySelector('#formLogin').addEventListener('submit', function(event) 
     });
 });
 
+passwordChange.addEventListener("change", function() {
+  if (this.checked) {
+    changePasswordForm.style.display = 'block';
+    oldPassword.setAttribute('required', '');
+    newPassword.setAttribute('required', '');
+    newPasswordConfirm.setAttribute('required', '');
+  } else {
+    changePasswordForm.style.display = 'none';
+    oldPassword.removeAttribute('required');
+    newPassword.removeAttribute('required');
+    newPasswordConfirm.removeAttribute('required');
+  }
+});
+
+
+
+dropdownlogout.onclick = function (){
+  overlayHome.style.display = "block";
+  overlayUser.style.display = "none";
+  Overlay.style.display = "none";
+  OverlayLogin.style.display = "none";
+  localStorage.clear();
+}
+function showdata(person){
+        var name = document.getElementById("name");
+        var email = document.getElementById("email");
+        var address = document.getElementById("address");
+        var phoneNumber = document.getElementById("phoneNumber");
+        var gender = document.getElementById("gender");
+        var dateOfBirth = document.getElementById("dateOfBirth");
+        //var password = document.getElementById("password");
+        //var passwordConfirm = document.getElementById("passwordConfirm");
+    
+        name.value = person.name;
+        email.value = person.email;
+        address.value = person.address;
+        phoneNumber.value = person.phone;
+        if(person.gender == true){
+            gender.value = "0";
+        }
+        else{gender.value = "1"}
+        const dateISO = person.birth.split('T')[0];
+        dateOfBirth.value = dateISO;
+        // password.value = person.password;
+        // passwordConfirm.value = person.password;
+    
+}
+function getInfo() {
+    fetch(`${url}/${Users}`, {
+        method: 'GET',
+        headers: {
+        'Authorization' : "bearer " + token
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        localStorage.setItem('id', data.id);
+        localStorage.setItem('roleName', data.roleName);
+        showdata(data);
+    })
+    .catch(error => {
+        console.error('Lỗi API: ', error);
+    });
+} 
+
+
+function handleOutsideClickInfo(event) {
+  if (!OverlaySignup.contains(event.target)) {
+    OverlaySignup.style.display = "none";
+    Overlay.style.display = "none";
+    OverlayInfor.style.display = "none";
+    document.removeEventListener("click", handleOutsideClickInfo, true);
+  }
+}
+dropdowninfo.addEventListener("click", function() {
+    Overlay.style.display = "block";
+    OverlayInfor.style.display = "block";
+    getInfo();
+    document.addEventListener("click", handleOutsideClickInfo, true);
+});
+
+document.querySelector('#formInfo').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+
+    if (!event.target.checkValidity()) {
+      event.target.classList.add('was-validated');
+      console.log("1");
+      return;
+    }
+    
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var Gender = document.getElementById("gender").value;
+    var birth = document.getElementById("dateOfBirth").value;
+    var oldPassword = document.getElementById("oldPassword").value;
+    var newPassword = document.getElementById("newPassword").value;
+    var newPasswordConfirm = document.getElementById("newPasswordConfirm").value;
+    var gender = false;
+    if(Gender == '0') {
+        gender = true;
+    }
+    if(newPassword != newPasswordConfirm && changePasswordForm.style.display == 'block'){
+      document.getElementById("newPasswordConfirm").classList.add("is-invalid");
+      console.log("2");
+      return;
+    }
+    
+    localStorage.setItem('put', 0);
+    fetch(`${url}/${Users}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : "bearer " + token
+      },
+      body: JSON.stringify({
+        id: localStorage.getItem('id'),
+        name: name,
+        email: email,
+        gender: gender,
+        birth: birth,
+        phone: phoneNumber,
+        address: address,
+        RoleName: localStorage.getItem('roleName')
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+          // showAlert("Chỉnh sửa thành công!");
+          
+        } else {
+          // showAlert("Chỉnh sửa thất bại!");
+          localStorage.setItem('put', 1);
+
+        }
+    })
+    .catch(error => {
+      localStorage.setItem('put', 1);
+      console.error(error);
+    });
+
+    if (changePasswordForm.style.display == 'block') {
+      fetch(`${url}/${Password}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization' : "bearer " + token
+        },
+        body: JSON.stringify({
+          id: localStorage.getItem('id'),
+          oldPassword: oldPassword,
+          newPassword: newPassword, 
+          RoleName: localStorage.getItem('roleName')
+        })
+      })
+      .then(response => {
+        if (response.ok) {
+            // showAlert("Thay đổi thành công!");
+          } else {
+            // showAlert("Thay đổi thất thất bại!");
+            localStorage.setItem('put', 2);
+          }
+      })
+      .catch(error => {
+        localStorage.setItem('put', 2);
+        console.error(error);
+      });
+    }     
+
+    if(localStorage.getItem('put') == 0){
+      showAlert("Chỉnh sửa thành công");
+    }else if(localStorage.getItem('put') == 1){
+      showAlert("Thông tin cá nhân không hợp lệ");
+    }
+    else{
+      showAlert("Thay đổi mật khẩu không thành công");
+    }
+  });
 
 
