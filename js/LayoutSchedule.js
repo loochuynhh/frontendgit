7883655a246200e90b7131f6b6e7581d7574ee6c
phuocduy <phuocduylpd108@gmail.com>
2023-05-10 18:36:20 +0700

    add layoutschedule

--- js/LayoutSchedule.js
+++ js/LayoutSchedule.js
@@ -1,5 +1,4 @@
-const URLSCHEDULE = ""
-window.onload = loadCalendar();
+window.onload = loadCalendar(); 
 
 function loadCalendar() {  
     var currentDate = new Date(); 
@@ -31,9 +30,13 @@ function loadCalendar() {
                 const buttons = document.querySelectorAll("#day-option .btn");
                 buttons.forEach(button => button.classList.remove('selectedButton'));
                 this.classList.add("selectedButton");
+                document.getElementById("film-for-select-schedule").innerHTML = "";
             // }
             const daySelected = date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, '0') + "-" +  dayOfMonth.toString().padStart(2, '0'); 
-            selectDay(daySelected);
+            const URLSCHEDULE = "https://localhost:44308/api/show?" + "date=" + daySelected +"&filmId=0&roomId=0";
+            // const URLSCHEDULE = "https://localhost:44308/api/show?" + "date=" + "2023-05-08" +"&filmId=0&roomId=0";
+            
+            selectDay(URLSCHEDULE);
         })
 
         document.getElementById("day-option").appendChild(btnDayOption);
@@ -42,7 +45,68 @@ function loadCalendar() {
     document.getElementById("center-month").innerHTML = "Tháng " + (currentDate.getMonth() + 1); 
 }
 
-function selectDay(dayOfMonth){
-    console.log(dayOfMonth)
+function selectDay(URLSCHEDULE){
+    const h4txtSelectSchedule = document.createElement("h4");
+    h4txtSelectSchedule.innerHTML = "Chọn lịch chiếu";
+    document.getElementById("film-for-select-schedule").appendChild(h4txtSelectSchedule);
+    // document.getElementById("film-for-select-schedule").setAttribute("min-height", "1000px");
+    
+    fetch(URLSCHEDULE)
+    .then(response => response.json())
+    .then(data => { 
+        if (data.length === 0){
+            const divNoSchedule =  document.createElement("div");
+            divNoSchedule.classList.add("text-center", "text-secondary");
+            divNoSchedule.innerHTML = "Ngày Bạn Chọn Hiện Không Có Lịch Chiếu Nào. Vui Lòng Chọn Ngày Khác.";
+            document.getElementById("film-for-select-schedule").appendChild(divNoSchedule);
+        }
+        console.log(data); 
+        for(let i = 0; i < data.length; i++){ 
+
+            console.log((data[i])[0].filmId);
+            const divFilm = document.createElement("div");
+            divFilm.classList.add("ms-4","mt-3","mb-3");
+
+            const h6Name = document.createElement("h6");
+            h6Name.classList.add("fw-bold", "text-uppercase");
+            h6Name.textContent = (data[i])[0].filmName;
+            console.log((data[i])[0].filmName);
+
+            const divFilmInfo= document.createElement("div");
+            divFilmInfo.classList.add("container", "d-flex");
+
+            const imgFilm = document.createElement("img");
+            imgFilm.setAttribute("width", "10%");
+            imgFilm.src = (data[i])[0].poster;
+
+            const divBtn = document.createElement("div");
+            divBtn.classList.add("ms-5");
+            data[i].forEach(element => { 
+                const startTime = new Date(element.startTime);
+                const sthours = startTime.getHours();
+                const stminutes = startTime.getMinutes(); 
+                const startTimeFormated = `${sthours.toString().padStart(2, '0')}:${stminutes.toString().padStart(2, '0')}`; 
+
+                const endTime = new Date(element.endTime);
+                const ethours = endTime.getHours();
+                const etminutes = endTime.getMinutes(); 
+                const endTimeFormated = `${ethours.toString().padStart(2, '0')}:${etminutes.toString().padStart(2, '0')}`;  
+
+                const btnSchedule = document.createElement("button");
+                btnSchedule.classList.add("btn", "btn-outline-primary", "m-2", "btn-sm", "text-center");
+                btnSchedule.innerHTML = startTimeFormated + " - " + endTimeFormated;
+                divBtn.appendChild(btnSchedule);
+            });
+
+            divFilmInfo.appendChild(imgFilm);
+            divFilmInfo.appendChild(divBtn);
+            divFilm.appendChild(h6Name);
+            divFilm.appendChild(divFilmInfo);
+
+            document.getElementById("film-for-select-schedule").appendChild(divFilm);
+
+        }  
+    })
+    .catch(error => console.error(error));
 }
 
