58aff19e7887904ed9b74ee22f9003b681d2b97e
phuocduy <phuocduylpd108@gmail.com>
2023-06-03 10:46:59 +0700

    clean admin

--- Admin/js/datatables-simple-demo.js
+++ Admin/js/datatables-simple-demo.js
@@ -1,6 +1,6 @@
-window.addEventListener('DOMContentLoaded', event => {
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
-});
+// window.addEventListener('DOMContentLoaded', event => {
+//     const datatablesSimple = document.getElementById('datatablesSimple');
+//     if (datatablesSimple) {
+//         new simpleDatatables.DataTable(datatablesSimple);
+//     }
+// });
