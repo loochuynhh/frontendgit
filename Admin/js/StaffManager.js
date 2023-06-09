209e4c731ce87835b2d2192b5085488e729046bd
loochuynhh <lochuynhhsb@gmail.com>
2023-06-09 20:30:18 +0700

    liti

--- Admin/js/StaffManager.js
+++ Admin/js/StaffManager.js
@@ -236,7 +236,7 @@ document.querySelector('#formSignup').addEventListener('submit', function (event
       position: 'top',
       icon: 'error',
       title: 'THẤT BẠI',
-      text: 'Tối thiểu 18 tuổi và năm sinh nhỏ hơn 1900',
+      text: 'Tối thiểu 18 tuổi và năm sinh sau 1900',
       // timer: 2000
     }).then(() => {
       document.addEventListener("click", handleOutsideClickAddStaff, true);
